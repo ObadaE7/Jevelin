0a9bd9e96672b3095452530ec3e0420fe218c8c9
Obada7E <obada.e.amer@gmail.com>
2024-06-23 10:36:40 +0300

    update section subscribe and make it responsive

--- resources/views/includes/index/subscribe.blade.php
+++ resources/views/includes/index/subscribe.blade.php
@@ -1,14 +1,16 @@
 <div class="main__subscribe">
     <div class="main__subscribe-form">
-        <span class="subscribe__text">{{ trans('string.Subscribe text') }}</span>
+        <span class="subscribe__text">{{ trans('index.sections.Subscribe text') }}</span>
         <form class="subscribe__form">
-            <input type="text" name="email" id="email" class="form-control"
-                placeholder="{{ trans('string.Enter your email address') }}">
-            <button class="main__hero-button">{{ trans('string.Subscribe') }}</button>
+            <input wire:model='email' type="text" name="email" id="email" class="form-control"
+                placeholder="{{ trans('index.sections.Email placeholder') }}">
+            <button wire:click.prevent='subscribe' class="main__hero-button">
+                {{ trans('index.sections.Subscribe') }}
+            </button>
         </form>
     </div>
 
     <div class="main__subscribe-img">
-        <img src="{{ asset('assets/img/svg/mailbox.svg') }}" alt="">
+        <img src="{{ asset('assets/img/svg/mailbox.svg') }}" alt="{{ trans('index.sections.Illustration') }}">
     </div>
 </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
         <main class="wrapper__main">
             @include('includes.index.hero')
             @include('includes.index.section-one')
-            {{--
             @include('includes.index.subscribe')
+            {{--
             @include('includes.index.section-two')
             @include('includes.index.our-best-user')
             @include('includes.index.section-three') --}}
