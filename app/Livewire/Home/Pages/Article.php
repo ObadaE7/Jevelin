51a4bc37108fcc5a3ab7554237b26d91848d2e27
Obada7E <obada.e.amer@gmail.com>
2024-06-26 21:16:46 +0300

    add article page (Not complete)

--- app/Livewire/Home/Pages/Article.php
+++ app/Livewire/Home/Pages/Article.php
@@ -2,12 +2,26 @@
 
 namespace App\Livewire\Home\Pages;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class Article extends Component
 {
+    public $slug;
+    public function mount($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    public function getArticle()
+    {
+        return Post::where('slug', $this->slug)->first();
+    }
+
     public function render()
     {
-        return view('pages.home.pages.article')->layout('layouts.guest');
+        $article = $this->getArticle();
+
+        return view('pages.home.pages.article', compact('article'))->layout('layouts.guest');
     }
 }
--- resources/views/pages/home/pages/article.blade.php
+++ resources/views/pages/home/pages/article.blade.php
@@ -1,3 +1,34 @@
-<div>
-صفخة المقال كامل
-</div>
+<section class="wrapper">
+    @livewire('home.header')
+
+    <main class="pages__wrapper">
+        <div class="pages__header">
+            <div class="d-flex align-items-center gap-2">
+                <span class="material-icons-outlined">feed</span>
+                <span class="fs-4">@lang('index.pages.Articles')</span>
+            </div>
+            <span class="text-muted">@lang('index.pages.Articles placeholder')</span>
+        </div>
+
+        <div class="pages__main article">
+            <div class="card">
+                <div class="card-body">
+                    <img src="{{ asset('storage/' . $article->image) }}" alt="{{ $article->slug }}">
+                    <div class="d-flex flex-column mb-3">
+                        <span class="fs-4">{{ $article->title }}</span>
+                        <small class="text-muted mb-3">{{ $article->title }}</small>
+
+                        <small class="text-muted">
+                            بواسطة: {{ $article->owner->fname . ' ' . $article->owner->lname }}
+                            <span class="ms-5">{{ $article->getDateForHuman() }}</span>
+                        </small>
+                    </div>
+
+                    <p>{{ $article->content }}</p>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    @livewire('home.footer')
+</section>
